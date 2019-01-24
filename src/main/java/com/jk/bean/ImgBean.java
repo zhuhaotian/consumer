007cf7ea9aebfb2248dbc9f4f963b7c8d45ef298
“zht <2865955965@qq.com>
2019-01-24 08:09:03 +0800

    第二次提交

---
+++ src/main/java/com/jk/bean/ImgBean.java
@@ -0,0 +1,13 @@
+package com.jk.bean;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class ImgBean implements Serializable {
+
+    private Integer id;
+
+    private String img;
+}
