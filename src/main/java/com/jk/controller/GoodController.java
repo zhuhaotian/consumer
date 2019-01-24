f2c506d10ce79dc69d35096edc0afece83a83094
HaoHuiLin <1091148413@qq.com>
2019-01-24 21:27:25 +0800

    购物车页面

---
+++ src/main/java/com/jk/controller/GoodController.java
@@ -0,0 +1,14 @@
+package com.jk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("goods")
+public class GoodController {
+
+    @RequestMapping("toGoods")
+    public String toGoods(){
+        return "goodshow";
+    }
+}
