8dfae24cb11c122ac4e162277a453da18e5692e8
xiaomiao525 <782303743@qq.com>
2019-01-26 14:03:41 +0800

    更新

--- src/main/java/com/jk/bean/ShopCar.java
+++ src/main/java/com/jk/bean/ShopCar.java
@@ -2,19 +2,31 @@ package com.jk.bean;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class ShopCar {
 
    private Integer Id;
+
    private String sku_mch;   //sku名称
+
    private Integer tjshl;   //添加数量
+
    private Integer  hj;   //合计
+
    private Integer yh_id;    //用户id
+
    private Integer  shp_id;   //商品id
-   private Integer chjshj;   //创建时间
+
+   private Date chjshj;   //创建时间
+
    private Integer sku_id;   //sku_id
-   private Integer shp_tp;   //商品图片
-   private Integer shfxz;
-   private Integer kcdz;
+
+   private String shp_tp;   //商品图片
+
+   private String shfxz;
+
+   private String kcdz;
 
 }
